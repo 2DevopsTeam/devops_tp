e695ade005b1604b22018a6a8fb8976398190798
Soufian AIT TIRITE <contact@soufian-a.net>
2020-03-13 10:19:44 +0100

    updates for env

--- docker-start.sh
+++ docker-start.sh
@@ -3,8 +3,12 @@
 echo "\n\n\nNpm install:"
 yarn install
 
+echo "\n\n\nAdd CORS:"
+adonis install @adonisjs/cors
+
 echo "\n\n\nRun migration:"
 adonis migration:run --force
 
 echo "\n\n\nStart node server:"
 adonis serve
+
